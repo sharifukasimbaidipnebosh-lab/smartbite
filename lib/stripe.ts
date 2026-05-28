8fbb64c9223fbd759224aeab99c91461e5026bb9
Sharifu Kasimba <your-sharifukasimbaidipnebosh@gmail.com>
2026-05-28 13:39:11 +0400

    fix stripe api version mismatch

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,13 +1,3 @@
 import Stripe from "stripe";
 
-export function getStripe() {
-  const key = process.env.STRIPE_SECRET_KEY;
-
-  if (!key) {
-    throw new Error("Missing STRIPE_SECRET_KEY");
-  }
-
-  return new Stripe(key, {
-    apiVersion: "2025-08-27.basil",
-  });
-}
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
