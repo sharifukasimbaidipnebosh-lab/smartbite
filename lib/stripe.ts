9a9f32e87b91848e77f527604d8edc5c1c2d50a9
Sharifu Kasimba <your-sharifukasimbaidipnebosh@gmail.com>
2026-05-29 11:01:56 +0400

    fix stripe production runtime initialization

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,16 +1,5 @@
-// @ts-ignore: stripe package may not have type declarations installed
 import Stripe from "stripe";
 
-declare const process: { env: { STRIPE_SECRET_KEY?: string } };
-
-let stripeInstance: Stripe | null = null;
-
-export function getStripe() {
-  if (!stripeInstance) {
-    stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2025-08-27.basil",
-    });
-  }
-
-  return stripeInstance;
-}
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-08-27.basil",
+});
